6b88926096d8a90843bb984e36daceb177207493
iamShah <formal>
2021-01-06 22:05:06 -0500

    jan 6

---
+++ enu.rb
@@ -0,0 +1,183 @@
+# basket = [1,2,3,4,5]
+
+# in_basket = basket.size
+# taken = 0
+
+# loop do 
+#     if taken < in_basket 
+#         puts "taking out #{basket[taken]}"
+#         taken +=1
+#     else
+#         break 
+#     end
+# end
+
+# basket = ["apple 1","apple 2","apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
+
+# apples_in_basket = basket.size # Step 1
+# apples_taken_out = 0 # Step 2
+
+# # Step 3 + 4
+# while apples_taken_out < apples_in_basket
+#     puts "Taking out #{basket[apples_taken_out]}"
+#     apples_taken_out += 1
+# end
+
+# 8.times do 
+#     puts "start studying!"
+# end
+
+
+# inputs = ""
+# while inputs != "Okkkk"
+#     puts "start studying!!!"
+#     inputs = gets.chomp
+# end
+
+
+# bro = ["a", "b", "c"]
+
+# count = 0
+
+# while count <= bro.length-1
+#     puts "start studying #{bro[count]}"
+#     count +=1
+# end
+
+# brother = ["a", "b", "c"]
+
+# brother.each do |aa|
+#     puts "start studying #{aa}"
+# end
+
+
+# color = ["red", "yellow", "orange"]
+
+# color.each do |color|
+#     puts "the color #{color} and its #{color.length} letters long."
+# end
+
+# arr = [1,2,3,4]
+
+# def aa(arr, e)
+#     puts arr << e    
+# end 
+
+# aa(arr, 444)
+
+# counts=12
+# while counts < 30
+#     puts "#{counts} seconds to new year"
+#         counts-=1
+#         sleep(1)
+# if counts==12
+#     puts"HAPPY NEW YEAR!"
+# end
+
+# end
+    
+# looping : do something a certain amount times 
+
+# iteration: if we have a collection of data, do something of each member of that data.
+
+# loop 1: how many 
+#      2: taken 
+#      3: start loop 
+#      4: if any left in the basket take one more 
+#      5: increment
+
+# basket = ["1", "2", "3", "4", "5"]
+
+# in_basket = basket.size 
+# taken = 0
+
+# loop do 
+#     if taken < in_basket
+
+#     puts "I'm taking out #{basket[taken]}"
+
+#     taken +=1 
+
+# else
+#     break
+# end
+# end 
+
+
+#  basket = [1,2,3,4,5]
+
+#  in_basket = basket.size 
+#  taken = 0 
+
+#  while taken < in_basket 
+#     puts "I'm taking out #{basket[taken]}"
+#     taken +=1
+
+#  end
+
+
+
+# basket = [1,2,3,4,5] 
+
+# basket.each do |apple| 
+#     puts "I'm taking out #{apple}"
+# end
+ 
+# 5.times do 
+#     puts "do something"
+# end
+
+# input = ""
+
+# while input != "OKKKK"
+#     puts "start studying"
+
+#     input = gets.chomp
+# end
+
+# brother = ["ahamd", "monir", "nawid"]
+
+# count = 0
+ 
+# while count <= brother.length-1
+     
+#     puts "start studying #{brother[count]}"
+
+#     count +=1
+
+# end
+
+# brother = ["ahamd", "monir", "nawid"]
+
+# brother.each do |andewalan|
+#     puts "start studying #{andewalan}"
+# end
+
+# original_color = ["orange", "red", "yellow", "blue"]
+
+# original_color.each do |color| 
+#     puts "original color #{color} length is #{color.size} letters long, and #{color} is my favourite color to be honest!!!"
+# end
+
+
+# def hi(name)
+#     puts "salam #{name}"
+# end
+
+# hi("ahmad")
+
+
+
+# brother = ["ahmad", "monir", "nawid"]
+
+# counter = 1 
+
+# brother.each do |bro|
+#     puts "this loop number #{counter}"
+#     puts "start studying #{bro}"
+#     counter +=1
+# end
+
+# brothers = ["ahmad", "monir", "nawid"]
+
+# brothers.each {|hadith| puts "hi #{hadith}"}
